a0a11d237e0ab7940a40e70016d95ff1a978e90f
mhwahdan <moustafawahdan55@gmail.com>
2021-07-23 19:54:32 +0200

    Beta

--- theBank/urls.py
+++ theBank/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('propertyMarket.urls'))
+    path('', include('GoldMark.urls'))
 ]
 
 if settings.DEBUG:
